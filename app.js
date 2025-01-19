4a1904518b160cde89effe10aeb527bf1512d06b
cxtlumon <cxt@currencies.co.uk>
2025-01-19 05:59:26 +0000

    adding controllers

--- app.js
+++ app.js
@@ -13,42 +13,8 @@ const crisController = new CrisController();
 const openaiController = new OpenaiController();
 const whatsappMiddleware = new WhatsappMiddleware();
 const whatsappController = new WhatsappController();
-app.get("/thread", (req, res) =>{
-    openaiController.createThreadWithAssistant().then((thread) =>{
-        res.json({threadId: thread.id});
-    }).catch((error) =>{
-        res.status(500).json({error: error.message});
-    });
-})
-app.post("/message", (req, res) =>{
-    const {threadId, message,affiliatedId, token} = req.body;
-    openaiController.addMessage(threadId, message).then( message => {
-        openaiController.runAssistant(threadId).then((response) =>{
-           const runId = response.id;
-           openaiController.pollingInterval = setInterval(() =>{
-            openaiController.checkingStatus(res, threadId, runId,affiliatedId, token);
-           }, 5000)
-          
-        }).catch((error) =>{
-            res.status(500).json({error: error.message});
-        });
-    }).catch((error) =>{
-        res.status(500).json({error: error.message});
-    });
-});
 app.post('/webhook', async (req, res) => {
     const payload = req.body;
-    console.log(req.body, 'request novamente')
-    console.log('Webhook payload received:', payload.entry[0].changes[0].value);
-    // if(payload.entry[0].changes[0].value.statuses){
-    //     const status = payload.entry[0].changes[0].value.statuses[0].status;
-    //     console.log(status, 'status')
-    //     if(status === 'sent'){
-    //         res.sendStatus(200);
-    //         return 
-    //     }
-    //     return;
-    // }
     const message = whatsappMiddleware._handlePayloadMessage(payload);
     const threadId = await openaiController.createThreadWithAssistant().then((thread) => {return thread.id});
     const response = await whatsappMiddleware.handleWebhookPayload(payload, threadId);
